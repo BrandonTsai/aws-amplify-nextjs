6405095f03c673ebef788359aaf3f146196a1d71
Brandon Tsai <brandon@darumatic.com>
2023-11-14 01:49:49 +1100

    try use client to fix frontend build fail issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,16 @@
+'use client';
 
 // import { Authenticator } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
 
 function Home() {
-    const handleGoogleLogin = async () => {
-        try {
-            await Auth.federatedSignIn({ provider: 'Google' });
-            console.log('Sign in with Google', error);
-        } catch (error) {
-            console.log('Error signing in with Google', error);
-        }
-    };
 
     return (
         <div>
             <h1>Welcome to my app!</h1>
-            {/* <Authenticator socialProviders={['google']} >
-                <main>
-                    <h1>Hello</h1>
-                </main>
-            </Authenticator> */}
-            <button onClick={handleGoogleLogin} style={{ backgroundColor: 'gray' }}>Login with Google</button>
-        </div>
+            <button onClick={() => Auth.signOut()}>Sign Out</button>
+            <button onClick={() => Auth.federatedSignIn({ provider: "Google" })}>Google Sign In</button>
+        </div >
     );
 }
 
