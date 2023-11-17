e79b599671998e91b0d9cfe921f8819e54706e21
Brandon Tsai <brandon@darumatic.com>
2023-11-17 22:00:17 +1100

    update auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client"
-import { withAuthenticator } from '@aws-amplify/ui-react';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
     return (
         <h1>Hello World</h1>
     );
 }
-// export default App
-export default withAuthenticator(App);
+export default App
+// export default withAuthenticator(App);
