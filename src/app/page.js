5c7fdbf3ab5282f67ddf8c9e03712f60c4c05925
Brandon Tsai <brandon@darumatic.com>
2023-11-17 23:12:15 +1100

    fix staging Access Denied issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 "use client"
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignInButton } from '@aws-amplify/ui-react';
 import "@aws-amplify/ui-react/styles.css";
 
 function App() {
     return (
-        <h1>Hello World</h1>
+        <AmplifyAuthenticator>
+            <AmplifySignInButton slot="federated" provider="Google" />
+            <h1>Hello World</h1>
+        </AmplifyAuthenticator>
     );
 }
 // export default App
+
 export default withAuthenticator(App);
